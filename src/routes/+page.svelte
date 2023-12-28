0cc99391ad4f161325afd57bf44ddbef3b8a6b75
Sara Pedersen <saraped99@gmail.com>
2023-12-28 14:44:01 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import NoteList from './Notelist.svelte';
+
+	let newNote = '';
+	let notes = ['My first note', 'My second note'];
+
+	function addNote() {
+		notes = [...notes, newNote];
+	}
+</script>
+
+<svelte:head>
+	<title>Notes</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<main class="notes">
+	<NoteList {notes} />
+
+	<div>
+		<!-- <input bind:value={newNote} type="text" placeholder="New note">
+        <button on:click={addNote}>Add</button> -->
+	</div>
+</main>
+
+<style lang="scss">
+	.notes {
+		display: grid;
+		grid-template-columns: 1fr 5fr;
+	}
+</style>
---
+++ src/routes/Notelist.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let notes;
+</script>
+
+<section class="notes">
+	<h1>Notes</h1>
+	<ul>
+		{#each notes as note}
+			<li>{note}</li>
+		{/each}
+	</ul>
+</section>
+
+<style>
+	.notes {
+	}
+</style>
